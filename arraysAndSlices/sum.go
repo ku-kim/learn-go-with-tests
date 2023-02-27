1e1e0c1b1268e28f64d6e47cf1cc070881f853c2
“kukim” <kukim.dev@gmail.com>
2023-02-27 14:27:28 +0900

    arrays: for range 문 사용

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -1,1 +1,11 @@
 package arraysAndSlices
+
+func Sum(numbers [5]int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
--- arraysAndSlices/sum_test.go
+++ arraysAndSlices/sum_test.go
@@ -1,1 +1,14 @@
 package arraysAndSlices
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	sut := Sum(numbers)
+
+	if sut != 15 {
+		t.Errorf("got %d want %d given, %v", sut, 15, numbers)
+	}
+
+}
