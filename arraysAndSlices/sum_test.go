08473cccfb47ea0a4fc7f96fb0cc67554afcf029
“kukim” <kukim.dev@gmail.com>
2023-02-28 14:40:21 +0900

    arraysAndSlices: SumAll 테스트 생성(red)

--- arraysAndSlices/sum_test.go
+++ arraysAndSlices/sum_test.go
@@ -24,5 +24,13 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
 
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
