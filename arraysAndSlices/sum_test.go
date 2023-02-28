258ea69dbb08d5fef0ea149f31d6d9845036740d
“kukim” <kukim.dev@gmail.com>
2023-02-28 15:43:30 +0900

    arraysAndSlices: sumAll, 가변인자와 reflect.DeepEqual (red)
    - reflectDeepEqual은 type safe 하지 않음

--- arraysAndSlices/sum_test.go
+++ arraysAndSlices/sum_test.go
@@ -1,6 +1,9 @@
 package arraysAndSlices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -28,9 +31,10 @@ func TestSum(t *testing.T) {
 
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
-	want := []int{3, 9}
+	//want := []int{3, 9}
+	want := "bob"
 
-	if got != want {
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
