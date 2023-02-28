85794865001bc5163628c2ac0ba600935bd8edbf
“kukim” <kukim.dev@gmail.com>
2023-02-28 14:21:38 +0900

    slices: Slice로 매개변수 받기

--- arraysAndSlices/sum_test.go
+++ arraysAndSlices/sum_test.go
@@ -3,12 +3,26 @@ package arraysAndSlices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	sut := Sum(numbers)
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if sut != 15 {
-		t.Errorf("got %d want %d given, %v", sut, 15, numbers)
-	}
+		sut := Sum(numbers)
+
+		if sut != 15 {
+			t.Errorf("got %d want %d given, %v", sut, 15, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 
 }
