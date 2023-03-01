c0a4a9d1400fe267b78727d8edb396cff3676177
“kukim” <kukim.dev@gmail.com>
2023-03-01 15:18:14 +0900

    struct: 구조체마다 메서드 생성(class와 유사) (green)

---
+++ structs/shapes.go
@@ -0,0 +1,24 @@
+package structs
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area(circle Circle) float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
